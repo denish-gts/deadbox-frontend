fae9d3a62c2c172838c92e6cff60dcc7b21b39f6
denish-gts <denish@glidetechsolutions.com>
2024-08-26 02:29:21 -0400

    add: group flow

--- src/component/group/GroupTab.tsx
+++ src/component/group/GroupTab.tsx
@@ -1,34 +1,49 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AllGroup from "./AllGroup";
 import Group from "./Group";
 import styles from "./tabs.module.scss";
+import RequestedMember from "./RequestedMember";
+import { usePathname, useRouter } from "next/navigation";
 
 function GroupTab() {
-  const [activeTab, setActiveTab] = useState("All-Group");
+  const path = usePathname();
+  const router = useRouter();
 
+  const [activeTab, setActiveTab] = useState("All-Group");
+  console.log('pathpathpathpathpathpath', path);
+
+  useEffect(() => {
+    setActiveTab(path)
+  }, [path])
   return (
     <>
       <div className="container">
         <div className={styles.tabsContainer}>
           <div>
             <div
-              className={`${styles.tab} ${activeTab === "All-Group" ? styles.active : ""}`}
-              onClick={() => setActiveTab("All-Group")}
+              className={`${styles.tab} ${activeTab === "/all-groups" ? styles.active : ""}`}
+              onClick={() => router.push("/all-groups")}
             >
               All Groups
             </div>
             <div
-              className={`${styles.tab} ${activeTab === "My-Group" ? styles.active : ""}`}
-              onClick={() => setActiveTab("My-Group")}
+              className={`${styles.tab} ${activeTab === "/group" ? styles.active : ""}`}
+              onClick={() => router.push("/group")}
             >
               My Group
+            </div>
+            <div
+              className={`${styles.tab} ${activeTab === "/requsted-member" ? styles.active : ""}`}
+              onClick={() => router.push("/requsted-member")}
+            >
+              Requested Member
             </div>
           </div>
         </div>
 
-        {activeTab === "All-Group" ? <AllGroup /> : <Group />}
+        {activeTab === "/all-groups" ? <AllGroup /> : activeTab === '/group' ? <Group /> : <RequestedMember />}
       </div>
     </>
   );
