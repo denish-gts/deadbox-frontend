c8c44c9a6ec4596e374577ab4bd4efd63d0e75f6
abhishek <abhishek@glidetechsolutions.com>
2024-11-25 09:41:20 +0530

    forgot password flow add

--- src/app/forgot-password/Components/forgotPasswordForm.tsx
+++ src/app/forgot-password/Components/forgotPasswordForm.tsx
@@ -9,6 +9,7 @@ import { errorCheckAPIResponse } from "@/utils/helpers";
 import Loader from "@/component/common/loader";
 import { toast } from "react-toastify";
 import { useRouter } from 'next/navigation';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 const Logo = "/assets/logo/logo.jpeg";
 const BG = "/assets/images/bg1.png";
 
@@ -19,32 +20,39 @@ const validationSchema = Yup.object().shape({
 });
 
 const validationSchema1 = Yup.object().shape({
-  password: Yup.string().required("Password is required."),
   reset_password_token: Yup.string().required("OTP is required."),
 
 });
+const validationSchema2 = Yup.object().shape({
+  password: Yup.string().required("Password is required."),
+  confirm_password: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm password is required'),
 
+});
 export default function ForgotPasswordForm() {
   const router = useRouter();
 
   const [isLoading, setIsLoading] = useState(false);
   const [step, setStep] = useState<Number>(1)
-  console.log('stepstepstepstepstepstep', step);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
   const { handleSubmit, values, touched, errors, handleChange } =
     useFormik({
-      initialValues: step === 1 ? {
+      initialValues:
+      {
         email: "",
-      } : {
         reset_password_token: '',
-        password: ''
+        password: '',
+        confirm_password: ''
       },
-      validationSchema: step === 1 ? validationSchema:validationSchema1,
+      validationSchema: step === 1 ? validationSchema : step === 2 ? validationSchema1 : validationSchema2,
       onSubmit: (values) => {
         setIsLoading(true);
         if (step === 1) {
           axiosInstance
-            .post(`auth/forgot-password`, values)
+            .post(`auth/forgot-password`, { email: values.email })
             .then((res) => {
               setStep(2)
               toast.success(res.data.data)
@@ -54,12 +62,25 @@ export default function ForgotPasswordForm() {
               setIsLoading(false);
               errorCheckAPIResponse(error);
             });
-        } else {
+        } else if (step === 2) {
           axiosInstance
-            .post(`auth/reset-password`, values)
+            .post(`auth/verify-reset-password-token`, { reset_password_token: values.reset_password_token })
+            .then((res) => {
+              setStep(3)
+
+              toast.success('OTP verify.')
+              setIsLoading(false);
+            })
+            .catch((error) => {
+              setIsLoading(false);
+              errorCheckAPIResponse(error);
+            });
+        } else if (step === 3) {
+          axiosInstance
+            .post(`auth/reset-password`, { email: values.email, password: values.password })
             .then((res) => {
               toast.success(res.data.data)
-              router.push('/firm-list')
+              router.push('/login')
               setIsLoading(false);
             })
             .catch((error) => {
@@ -80,7 +101,7 @@ export default function ForgotPasswordForm() {
         <div className={styles.logo}>
           <Image src={Logo} alt="Logo" unoptimized height={0} width={0} />
         </div>
-        <h2>Forgot Password</h2>
+        <h2>{step === 1 ? 'Forgot Password' : step === 2 ? 'Verify OTP' : 'Reset Password'}</h2>
         {step === 1 && (
           <form onSubmit={handleSubmit}>
             <input
@@ -102,16 +123,10 @@ export default function ForgotPasswordForm() {
                 {errors.email}
               </p>
             ) : null}
-            {/* {isLoading ? (
-            <button style={{ backgroundColor: "#9e9e9e" }} type="button">
-              Submit
-            </button>
-          ) : ( */}
             <button type="submit">Send OTP</button>
             <p style={{ marginTop: '10px' }} className={styles.center}>
-            Already have an account? <a href="/login">Login</a>
+              Already have an account? <a href="/login">Login</a>
             </p>
-            {/* )} */}
           </form>
         )}
         {step === 2 && (
@@ -134,13 +149,31 @@ export default function ForgotPasswordForm() {
                 {errors.reset_password_token}
               </p>
             ) : null}
-            <input
-              type="text"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-              value={values.password}
-            />
+
+            <button type="submit">{'Verify OTP'}</button>
+            <p style={{ marginTop: '10px' }} className={styles.center}>
+              Already have an account? <a href="/login">Login</a>
+            </p>
+          </form>
+
+        )}
+        {step === 3 && (
+          <form onSubmit={handleSubmit}>
+            <div className={styles.passwordField}>
+              <input
+                type={passwordVisible ? "text" : "password"}
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+                value={values.password}
+              />
+              <span
+                className={styles.eyeIcon}
+                onClick={() => setPasswordVisible(!passwordVisible)}
+              >
+                {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
             {errors.password && touched.password ? (
               <p className={styles.error_content}
                 style={{
@@ -152,15 +185,35 @@ export default function ForgotPasswordForm() {
                 {errors.password}
               </p>
             ) : null}
-            {/* {isLoading ? (
-            <button style={{ backgroundColor: "#9e9e9e" }} type="button">
-              Submit
-            </button>
-          ) : ( */}
-            <button type="submit">{step == 1 ? 'Send OTP' : 'Submit'}</button>
-            {/* )} */}
+              <div className={styles.passwordField}>
+              <input
+                type={confirmPasswordVisible ? "text" : "password"}
+                placeholder="Confirm Password"
+                name="confirm_password"
+                onChange={handleChange}
+                value={values.confirm_password}
+              />
+              <span
+                className={styles.eyeIcon}
+                onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
+              >
+                {confirmPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
+            {errors.confirm_password && touched.confirm_password ? (
+              <p className={styles.error_content}
+                style={{
+                  color: "red",
+                  fontSize: "12px",
+                  marginTop: "-10px",
+                  marginBottom: "16px",
+                }}>
+                {errors.confirm_password}
+              </p>
+            ) : null}
+            <button type="submit">{'Submit'}</button>
             <p style={{ marginTop: '10px' }} className={styles.center}>
-            Already have an account? <a href="/login">Login</a>
+              Already have an account? <a href="/login">Login</a>
             </p>
           </form>
 
