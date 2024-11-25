a2398467dd39ff7c06eb19e489e2e45ee50ddb48
abhishek <abhishek@glidetechsolutions.com>
2024-11-25 12:17:10 +0530

    add: some cosmetic issue

--- src/component/group/AddGroup.tsx
+++ src/component/group/AddGroup.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "./addGroup.module.scss";
 import Image from "next/image";
 import { useFormik } from "formik";
@@ -9,9 +9,11 @@ import { errorCheckAPIResponse, successAPIResponse } from "@/utils/helpers";
 import classNames from "classnames";
 import { useRouter } from "next/navigation";
 import Loader from "../common/loader";
+import Select from 'react-select';
 
 import Pencil from "../../../public/assets/images/pencils.png";
 import Delete from "../../../public/assets/images/delete.svg";
+import { MainContent } from "@/utils/context";
 interface User {
   id: number;
   name: string;
@@ -40,7 +42,7 @@ const validationSchema1 = Yup.object().shape({
 
 export default function AddGroup({ header, groupId, type }) {
   const router = useRouter();
-
+  const { sassRoles } = useContext(MainContent);
   const [isLoading, setIsLoading] = useState(false);
   const [showModel, setShowModel] = useState(false);
   const [peopleId, setPeopleId] = useState("");
@@ -69,8 +71,8 @@ export default function AddGroup({ header, groupId, type }) {
           apiData.append("phone", values.phone);
           apiData.append("phone_code", values.phone_code);
           apiData.append("email", values.email);
-        }else{
-          apiData.append("id", parseInt(groupId) as any);    
+        } else {
+          apiData.append("id", parseInt(groupId) as any);
         }
         apiData.append("description", values.about);
 
@@ -86,7 +88,7 @@ export default function AddGroup({ header, groupId, type }) {
           )
         );
         const URL = type === 'edit_group' ? 'group/update' : 'group/create'
-        const message = type === 'edit_group' ? "Your group is update.": "Your group is created and sent for the approval to the admin"
+        const message = type === 'edit_group' ? "Your group is update." : "Your group is created and sent for the approval to the admin"
 
         postFormData(URL, apiData)
           .then((res) => {
@@ -94,7 +96,8 @@ export default function AddGroup({ header, groupId, type }) {
             const msg = {
               data: {
                 message:
-                message              },
+                  message
+              },
             };
             successAPIResponse(msg);
             setIsLoading(false);
@@ -144,7 +147,8 @@ export default function AddGroup({ header, groupId, type }) {
             groupName: responce.group.title,
             invitees: responce.member_list,
             about: responce.group.description,
-            avatar: responce.group.logo
+            avatar: responce.group.logo,
+            groupType: responce.group.group_type
           }
           setAvatar(responce.group.logo)
           setValues({ ...values, ...getData });
@@ -157,70 +161,14 @@ export default function AddGroup({ header, groupId, type }) {
     }
   }, [groupId]);
 
-  // const [members, setMembers] = useState([
-  //   {
-  //     name: "Marvin McKinney",
-  //     email: "marvinmckinney@gmail.com",
-  //     activeOn: "17/07/2024",
-  //     role: "Member",
-  //     roleColor: "#6ccf51", // Green
-  //     avatar: "path/to/avatar1.jpg",
-  //   },
-  //   {
-  //     name: "Jerome Bell",
-  //     email: "jeromebell@gmail.com",
-  //     activeOn: "16/07/2024",
-  //     role: "President",
-  //     roleColor: "#f44336", // Red
-  //     avatar: "path/to/avatar2.jpg",
-  //   },
-  //   {
-  //     name: "Bessie Cooper",
-  //     email: "bessiecooper@gmail.com",
-  //     activeOn: "16/07/2024",
-  //     role: "Recruiter",
-  //     roleColor: "#ffa726", // Orange
-  //     avatar: "path/to/avatar3.jpg",
-  //   },
-  //   {
-  //     name: "Brooklyn Simmons",
-  //     email: "brooklynsimmons@gmail.com",
-  //     activeOn: "16/07/2024",
-  //     role: "Squad-Leader",
-  //     roleColor: "#ffa726", // Orange
-  //     avatar: "path/to/avatar4.jpg",
-  //   },
-  //   {
-  //     name: "Guy Hawkins",
-  //     email: "guyhawkins@gmail.com",
-  //     activeOn: "15/07/2024",
-  //     role: "Vice President",
-  //     roleColor: "#f44336", // Red
-  //     avatar: "path/to/avatar5.jpg",
-  //   },
-  // ]);
-
-  // const roles = [
-  //   "Member",
-  //   "President",
-  //   "Recruiter",
-  //   "Squad-Leader",
-  //   "Vice President",
-  // ];
-
-  // const handleRoleChange = (index, newRole) => {
-  //   const updatedMembers = members.map((member, i) =>
-  //     i === index ? { ...member, role: newRole } : member
-  //   );
-  //   setMembers(updatedMembers);
-  // };
+ 
   const DadboxRoles = [
     {
-      lable: 'Admin',
+      label: 'Admin',
       value: 1
     },
     {
-      lable: 'Member',
+      label: 'Member',
       value: 2,
       color: 'green'
     }]
@@ -228,14 +176,15 @@ export default function AddGroup({ header, groupId, type }) {
   const handleChangeRole = (e: any, index: number) => {
     const updateData = values?.invitees?.map((item: any, i: number) => {
       if (i === index) {
-        return { ...item, role_id: e.target.value }
+        return { ...item, role_id: e }
       } else {
         return item
       }
     })
     setValues({ ...values, invitees: updateData });
   }
-  const RolesData = DadboxRoles
+  const RolesData = values?.groupType === 'saas' ? sassRoles : DadboxRoles
+
   return (
     <>
       <div className="container">
@@ -332,8 +281,12 @@ export default function AddGroup({ header, groupId, type }) {
                     onChange={handleChange}
                     value={values.groupType}
                   >
-                    {/* <option value="deadbox">Deadbox</option>
-                    <option value="saas">Saas</option> */}
+                    {type === 'edit_group' && (
+                      <>
+                        <option value="deadbox">Deadbox</option>
+                        <option value="saas">Saas</option>
+                      </>
+                    )}
                     <option value="general_private">General Private</option>
                     <option value="general_public">General Public</option>
                   </select>
@@ -585,31 +538,39 @@ export default function AddGroup({ header, groupId, type }) {
                         </tr>
                       </thead>
                       <tbody>
-                        {values.invitees.map((invitee, key) => (
-                          <tr key={key}>
-                            <td>
-                              <div className={styles.memberInfo}>
-                                <Image
-                                  src={invitee.image}
-                                  alt="people1"
-                                  height={0}
-                                  width={0}
-                                  unoptimized
-                                />
-                                <span>{invitee?.first_name} {invitee?.last_name}</span>
-                              </div>
-                            </td>
-                            <td>{invitee.email}</td>
-                            <td>
-                              <div className={styles.roleDropdown}>
-                                <div className={styles.role}>
-                                  <span
-                                    className={styles.roleIndicator}
-                                    style={{
-                                      backgroundColor: invitee.roleColor,
-                                    }}
-                                  ></span>
-                                  <select
+                        {values.invitees.map((invitee, key) => {
+                          const roleId = invitee?.role_id.toString().split(',')
+                          const valuesData = RolesData.filter((item: any) => {
+                            if (roleId.includes(item.value.toString())) {
+                              return item
+                            }
+                          })                          
+                          return (
+
+                            <tr key={key}>
+                              <td>
+                                <div className={styles.memberInfo}>
+                                  <Image
+                                    src={invitee.image}
+                                    alt="people1"
+                                    height={0}
+                                    width={0}
+                                    unoptimized
+                                  />
+                                  <span>{invitee?.first_name} {invitee?.last_name}</span>
+                                </div>
+                              </td>
+                              <td>{invitee.email}</td>
+                              <td>
+                                <div className={styles.roleDropdown}>
+                                  <div className={styles.role}>
+                                    <span
+                                      className={styles.roleIndicator}
+                                      style={{
+                                        backgroundColor: invitee.roleColor,
+                                      }}
+                                    ></span>
+                                    {/* <select
                                     value={invitee.role_id}
                                     onChange={(e) => {
                                       handleChangeRole(e, key)
@@ -617,58 +578,77 @@ export default function AddGroup({ header, groupId, type }) {
 
                                   >
                                     {RolesData.map((role, i) => (
+                                      <>
+                                      {console.log('dsdsdsdsdsdsd',role)
+                                      }
                                       <option
                                         key={i}
                                         value={role?.value}
                                       >
                                         {role?.lable}
                                       </option>
+                                      </>
                                     ))}
-                                  </select>
-                                </div>
-                              </div>
-                            </td>
-                            <td>
-                              <button className={styles.deleteButton}>
-                                <svg
-                                  width="19"
-                                  height="19"
-                                  viewBox="0 0 19 19"
-                                  fill="none"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  onClick={() => {
-                                    setShowModel(true);
-                                    setPeopleId(invitee.id);
-                                  }}
-                                >
-                                  <g clip-path="url(#clip0_101_535)">
-                                    <path
-                                      d="M16.0999 3.06667H13.7232C13.5453 2.20142 13.0745 1.42397 12.3902 0.86535C11.7059 0.306736 10.8499 0.00111514 9.96654 0L8.4332 0C7.54985 0.00111514 6.69388 0.306736 6.00958 0.86535C5.32527 1.42397 4.85448 2.20142 4.67654 3.06667H2.29987C2.09654 3.06667 1.90153 3.14744 1.75775 3.29122C1.61398 3.435 1.5332 3.63 1.5332 3.83333C1.5332 4.03667 1.61398 4.23167 1.75775 4.37545C1.90153 4.51923 2.09654 4.6 2.29987 4.6H3.06654V14.5667C3.06775 15.583 3.47201 16.5573 4.19064 17.2759C4.90926 17.9945 5.88358 18.3988 6.89987 18.4H11.4999C12.5162 18.3988 13.4905 17.9945 14.2091 17.2759C14.9277 16.5573 15.332 15.583 15.3332 14.5667V4.6H16.0999C16.3032 4.6 16.4982 4.51923 16.642 4.37545C16.7858 4.23167 16.8665 4.03667 16.8665 3.83333C16.8665 3.63 16.7858 3.435 16.642 3.29122C16.4982 3.14744 16.3032 3.06667 16.0999 3.06667ZM8.4332 1.53333H9.96654C10.4421 1.53391 10.9058 1.68159 11.2941 1.95611C11.6824 2.23062 11.9763 2.61854 12.1354 3.06667H6.2643C6.42345 2.61854 6.71733 2.23062 7.10563 1.95611C7.49394 1.68159 7.95766 1.53391 8.4332 1.53333ZM13.7999 14.5667C13.7999 15.1767 13.5576 15.7617 13.1262 16.193C12.6949 16.6243 12.1099 16.8667 11.4999 16.8667H6.89987C6.28987 16.8667 5.70486 16.6243 5.27352 16.193C4.84219 15.7617 4.59987 15.1767 4.59987 14.5667V4.6H13.7999V14.5667Z"
-                                      fill="black"
-                                    />
-                                    <path
-                                      d="M7.66657 13.8001C7.8699 13.8001 8.06491 13.7193 8.20868 13.5755C8.35246 13.4318 8.43324 13.2367 8.43324 13.0334V8.43341C8.43324 8.23008 8.35246 8.03508 8.20868 7.8913C8.06491 7.74752 7.8699 7.66675 7.66657 7.66675C7.46324 7.66675 7.26823 7.74752 7.12445 7.8913C6.98068 8.03508 6.8999 8.23008 6.8999 8.43341V13.0334C6.8999 13.2367 6.98068 13.4318 7.12445 13.5755C7.26823 13.7193 7.46324 13.8001 7.66657 13.8001Z"
-                                      fill="black"
-                                    />
-                                    <path
-                                      d="M10.7335 13.8001C10.9368 13.8001 11.1318 13.7193 11.2756 13.5755C11.4194 13.4318 11.5001 13.2367 11.5001 13.0334V8.43341C11.5001 8.23008 11.4194 8.03508 11.2756 7.8913C11.1318 7.74752 10.9368 7.66675 10.7335 7.66675C10.5301 7.66675 10.3351 7.74752 10.1913 7.8913C10.0476 8.03508 9.9668 8.23008 9.9668 8.43341V13.0334C9.9668 13.2367 10.0476 13.4318 10.1913 13.5755C10.3351 13.7193 10.5301 13.8001 10.7335 13.8001Z"
-                                      fill="black"
-                                    />
-                                  </g>
-                                  <defs>
-                                    <clipPath id="clip0_101_535">
-                                      <rect
-                                        width="18.4"
-                                        height="18.4"
-                                        fill="white"
+                                  </select> */}
+                                    <div className={styles.roleSelect}>
+                                      <Select
+                                        closeMenuOnSelect={false}
+                                        isMulti={RolesData.length > 2}
+                                        value={valuesData}
+                                        onChange={(value: any) => {
+                                          const data = RolesData.length > 2 ? value : [value]
+                                          const role_id = data?.map((item: any) => { return item.value }).toString()
+                                          handleChangeRole(role_id, key)
+                                        }}
+
+                                        options={RolesData}
                                       />
-                                    </clipPath>
-                                  </defs>
-                                </svg>
-                              </button>
-                            </td>
-                          </tr>
-                        ))}
+                                    </div>
+                                  </div>
+                                </div>
+                              </td>
+                              <td>
+                                <button className={styles.deleteButton}>
+                                  <svg
+                                    width="19"
+                                    height="19"
+                                    viewBox="0 0 19 19"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    onClick={() => {
+                                      setShowModel(true);
+                                      setPeopleId(invitee.id);
+                                    }}
+                                  >
+                                    <g clip-path="url(#clip0_101_535)">
+                                      <path
+                                        d="M16.0999 3.06667H13.7232C13.5453 2.20142 13.0745 1.42397 12.3902 0.86535C11.7059 0.306736 10.8499 0.00111514 9.96654 0L8.4332 0C7.54985 0.00111514 6.69388 0.306736 6.00958 0.86535C5.32527 1.42397 4.85448 2.20142 4.67654 3.06667H2.29987C2.09654 3.06667 1.90153 3.14744 1.75775 3.29122C1.61398 3.435 1.5332 3.63 1.5332 3.83333C1.5332 4.03667 1.61398 4.23167 1.75775 4.37545C1.90153 4.51923 2.09654 4.6 2.29987 4.6H3.06654V14.5667C3.06775 15.583 3.47201 16.5573 4.19064 17.2759C4.90926 17.9945 5.88358 18.3988 6.89987 18.4H11.4999C12.5162 18.3988 13.4905 17.9945 14.2091 17.2759C14.9277 16.5573 15.332 15.583 15.3332 14.5667V4.6H16.0999C16.3032 4.6 16.4982 4.51923 16.642 4.37545C16.7858 4.23167 16.8665 4.03667 16.8665 3.83333C16.8665 3.63 16.7858 3.435 16.642 3.29122C16.4982 3.14744 16.3032 3.06667 16.0999 3.06667ZM8.4332 1.53333H9.96654C10.4421 1.53391 10.9058 1.68159 11.2941 1.95611C11.6824 2.23062 11.9763 2.61854 12.1354 3.06667H6.2643C6.42345 2.61854 6.71733 2.23062 7.10563 1.95611C7.49394 1.68159 7.95766 1.53391 8.4332 1.53333ZM13.7999 14.5667C13.7999 15.1767 13.5576 15.7617 13.1262 16.193C12.6949 16.6243 12.1099 16.8667 11.4999 16.8667H6.89987C6.28987 16.8667 5.70486 16.6243 5.27352 16.193C4.84219 15.7617 4.59987 15.1767 4.59987 14.5667V4.6H13.7999V14.5667Z"
+                                        fill="black"
+                                      />
+                                      <path
+                                        d="M7.66657 13.8001C7.8699 13.8001 8.06491 13.7193 8.20868 13.5755C8.35246 13.4318 8.43324 13.2367 8.43324 13.0334V8.43341C8.43324 8.23008 8.35246 8.03508 8.20868 7.8913C8.06491 7.74752 7.8699 7.66675 7.66657 7.66675C7.46324 7.66675 7.26823 7.74752 7.12445 7.8913C6.98068 8.03508 6.8999 8.23008 6.8999 8.43341V13.0334C6.8999 13.2367 6.98068 13.4318 7.12445 13.5755C7.26823 13.7193 7.46324 13.8001 7.66657 13.8001Z"
+                                        fill="black"
+                                      />
+                                      <path
+                                        d="M10.7335 13.8001C10.9368 13.8001 11.1318 13.7193 11.2756 13.5755C11.4194 13.4318 11.5001 13.2367 11.5001 13.0334V8.43341C11.5001 8.23008 11.4194 8.03508 11.2756 7.8913C11.1318 7.74752 10.9368 7.66675 10.7335 7.66675C10.5301 7.66675 10.3351 7.74752 10.1913 7.8913C10.0476 8.03508 9.9668 8.23008 9.9668 8.43341V13.0334C9.9668 13.2367 10.0476 13.4318 10.1913 13.5755C10.3351 13.7193 10.5301 13.8001 10.7335 13.8001Z"
+                                        fill="black"
+                                      />
+                                    </g>
+                                    <defs>
+                                      <clipPath id="clip0_101_535">
+                                        <rect
+                                          width="18.4"
+                                          height="18.4"
+                                          fill="white"
+                                        />
+                                      </clipPath>
+                                    </defs>
+                                  </svg>
+                                </button>
+                              </td>
+                            </tr>
+                          )
+                        })}
                       </tbody>
                     </table>
                   </div>
--- src/shared/wrapper/LayoutCustom.tsx
+++ src/shared/wrapper/LayoutCustom.tsx
@@ -1,24 +1,42 @@
 "use client";
 
+import { post } from "@/api/base";
 import Footer from "@/component/Footer/Footer";
 import Header from "@/component/header/Header";
 import Tabs from "@/component/home/tab";
 import { authenticate, unProtectedRoute } from "@/utils/auth.util";
+import { MainContent } from "@/utils/context";
+
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const LayoutCustom = ({ children }) => {
   const path = `/${usePathname().split('/')[1]}`;
+  const [sassRoles, setSassRoles] = useState([])
 
   const router = useRouter();
-  const loginRoutes = ["/login",'/forgot-password', "/signup",,'/signup-2', '/magic', '/verifyemail','/magic-link', '/','/verify-email'];
+  const loginRoutes = ["/login", '/forgot-password', "/signup", , '/signup-2', '/magic', '/verifyemail', '/magic-link', '/', '/verify-email'];
   const login = loginRoutes.includes(path);
-  const protectedRoutes = ["/profile",'/edit-group', '/edit-profile', '/group', '/add-group', '/all-groups', '/requsted-member'];
+  const protectedRoutes = ["/profile", '/edit-group', '/edit-profile', '/group', '/add-group', '/all-groups', '/requsted-member'];
   const protectedRoute = protectedRoutes.includes(path);
 
   useEffect(() => {
-    console.log('xsdsdsdsddsd',!authenticate() ,path, !unProtectedRoute.includes(path),);
-    
+    console.log('xsdsdsdsddsd', !authenticate(), path, !unProtectedRoute.includes(path),);
+    const body = { for_sas: 1 }
+    post(`master/group-role/list`, body)
+      .then((data: any) => {
+        const responce = data.data.data;
+        const SASData = responce.map((item: any) => {
+          return ({
+            label: item?.title,
+            value: item?.id,
+            color: item?.color
+          })
+        })
+        setSassRoles(SASData)
+      })
+      .catch((error) => {
+      });
     if (authenticate()) {
       if ([...unProtectedRoute].includes(path)) {
         router.push(`/profile`)
@@ -30,14 +48,16 @@ export const LayoutCustom = ({ children }) => {
   return (
     <>
       {protectedRoute && (
-        <div>
-          <Header />
-          <div className="main-bg">
-            <Tabs />
-            {children}
+        <MainContent.Provider value={{ sassRoles }}>
+          <div>
+            <Header />
+            <div className="main-bg">
+              <Tabs />
+              {children}
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
+        </MainContent.Provider>
       )}
       {login && <>{children}</>}
     </>
